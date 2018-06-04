086a407ed62b163be85021ad8e1fa51d7a8fc627
Vincent_Lin <31683368+give0223@users.noreply.github.com>
2018-06-05 01:28:22 +0800

    ajax fix

--- src/js/channel_main.js
+++ src/js/channel_main.js
@@ -4,26 +4,27 @@ function getData(callback) {
     var client_id = 'tlhkyl4jw6dvwl4lamhmcll4aq79ur';
     //最大回傳資料數
     var limit = '9';
+    var lang = 'zh-TW'
     //URL
     var Url = `https://api.twitch.tv/kraken/streams/?
-               &stream_type=live&limit=${limit}`;   
+               &stream_type=live&language=${lang}&limit=${limit}`;   
     var xhr = new XMLHttpRequest();
     xhr.open('GET', Url, true);
     xhr.setRequestHeader('Client-ID', client_id);
     xhr.send();
     xhr.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
-            var data = JSON.parse(this.responseText);
-            callback(null, data);
+            var response = JSON.parse(this.responseText);
+            callback(null, response);
         }
     };
 }
 
 function appendData() {
-    getData(null,function(error, data) {
-        console.log("err: " + error + " , getdata: " + data);
+    getData(function(error, data) {
+        var Streams = data.streams;
+        
     });
 }
 
-// getData();
 appendData();
