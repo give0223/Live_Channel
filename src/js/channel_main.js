18ff4752a1b23330b7526a7861e5e498e01a2130
Vincent_Lin <give0223@gmail.com>
2018-06-05 17:58:14 +0800

    js add col

--- src/js/channel_main.js
+++ src/js/channel_main.js
@@ -4,6 +4,7 @@ function getData(callback) {
     var client_id = 'tlhkyl4jw6dvwl4lamhmcll4aq79ur';
     //最大回傳資料數
     var limit = '9';
+    //語言
     var lang = 'zh-TW'
     //URL
     var Url = `https://api.twitch.tv/kraken/streams/?
@@ -20,21 +21,49 @@ function getData(callback) {
     };
 }
 
+//呼叫getData函式取得Twitch資料，再將取得資料放入HTML中
 function appendData() {
     getData(function(error, data) {
         var Streams = data.streams;
-        ViewCol(Streams);
+        //宣告取得主畫面最外層框架
+        var main_row = document.querySelector('.channel_row');
+        //將每個頻道依序加入框架中
+        Streams.forEach(item => {
+            var channel_col = ViewCol(item);
+            main_row.innerHTML += channel_col;
+        });
+        
+        
     });
 }
 
 //建立頻道畫面
 function ViewCol(StreamData) {
-    var row = document.querySelector('.channel_row');
+    return `
+            <div class='col'>
+                <a href='${StreamData.channel.url}' target='_blank' title='${StreamData.channel.status}'>
+                    <div class='preview'>
+                        <img src='${StreamData.preview.medium}'>
+                    </div>
+                    <div class='button'>
+                        <div class='avator'>
+                            <img src='${StreamData.channel.logo}'>
+                        </div>
+                        <div class='intor'>
+                            <div class='channel_name'>
+                                ${StreamData.channel.status}
+                            </div>
+                            <div class='owner_name'>
+                                ${StreamData.channel.display_name}
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>`;
     
 }
 
-
-var bodyload = document.querySelector('body');
-bodyload.onload = function() {
+//Loading
+$(window).ready(function() {
     appendData();
-};
+});
